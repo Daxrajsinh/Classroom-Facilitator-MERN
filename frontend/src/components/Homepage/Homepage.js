8b32444b5eaeeea9f8fbafb23de61acd43733279
Daxrajsinh <daxrajsinh16@gmail.com>
2024-03-06 00:12:46 +0530

    Footer changed !

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -44,42 +44,43 @@ function Homepage() {
                             </div>
                         </div>
                         <div className="col-lg-5 col-md-12 col-xs-12 mx-3">
-                        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-    <ol className="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div className="carousel-inner">
-        <div className="carousel-item active">
-            <img className="d-block w-100 carousel-image" src="https://i.pinimg.com/564x/e8/80/25/e8802514ddad04c96f1235cc208ba247.jpg" alt="First slide"/>
-        </div>
-        <div className="carousel-item">
-            <img className="d-block w-100 carousel-image" src="https://st2.depositphotos.com/5178011/8137/v/450/depositphotos_81374190-stock-illustration-doubting-man.jpg" alt="Second slide"/>
-        </div>
-        <div className="carousel-item">
-            {/* <img className="d-block w-100 carousel-image" src="https://i1.wp.com/blog.doubtbuddy.com/wp-content/uploads/2021/10/doubttt.jpg?resize=970%2C667&ssl=1" alt="Third slide"/> */}
-            <img className="d-block w-100 carousel-image" src="https://cdn.vectorstock.com/i/1000x1000/89/60/cute-kid-thinking-idea-on-white-background-vector-47088960.webp" alt="Third slide"/>
-        </div>
-    </div>
-    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-    </a>
-    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-    </a>
-</div>
+                            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="2000">
+
+                                <ol className="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img className="d-block w-100 carousel-image" src="https://i.pinimg.com/474x/6f/8c/e5/6f8ce50f21889cb98afa9021233e9653.jpg" alt="First slide" />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img className="d-block w-100 carousel-image" src="https://st2.depositphotos.com/5178011/8137/v/450/depositphotos_81374190-stock-illustration-doubting-man.jpg" alt="Second slide" />
+                                    </div>
+                                    <div className="carousel-item">
+                                        {/* <img className="d-block w-100 carousel-image" src="https://i1.wp.com/blog.doubtbuddy.com/wp-content/uploads/2021/10/doubttt.jpg?resize=970%2C667&ssl=1" alt="Third slide"/> */}
+                                        <img className="d-block w-100 carousel-image" src="https://cdn.vectorstock.com/i/1000x1000/89/60/cute-kid-thinking-idea-on-white-background-vector-47088960.webp" alt="Third slide" />
+                                    </div>
+                                </div>
+                                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>
+                            </div>
 
                         </div>
                     </div>
                 </div>
             </header>
             {/* <footer className="text-center text-lg-start" Style="background-color: #4e63d7; position:absolute; bottom:0vh;width:100%;"> */}
-            <footer className="text-center text-lg-start" Style="background-color: #4e63d7; position:absolute; bottom:0vh;width:100%;">
-                <div className="text-center text-white p-3" Style="background-color: rgb(25, 113, 2)">
-                    Made With ❤ by Doubtout (© 2024 )
+            <footer className="text-center" Style="background-color: rgb(2, 113, 2); position:absolute; bottom:0vh;width:100%;">
+                <div className="text-center text-white p-3" Style="background-color: rgba(82, 102, 58, 0.05);">
+                    Made With ♡ by Doubtout (© 2024 )
                 </div>
             </footer>
         </>
